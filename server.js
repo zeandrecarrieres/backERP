21858e5d8ff3b692f68cedd94a0ab014831a16c8
Zé Carrières <zeandredev@gmail.com>
2021-04-22 21:57:06 +0200

    update cors

--- server.js
+++ server.js
@@ -25,10 +25,11 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const app = express();
 app.use(cookieParser())
-app.use(cors({
-  credentials:true,
-  origin: ['http://localhost:3001', 'http://localhost:3000']
-}));
+app.use(cors())
+// app.use(cors({
+//   credentials:true,
+//   origin: ['http://localhost:3001', 'http://localhost:3000']
+// }));
 app.use(express.json());
 
 //Routes
